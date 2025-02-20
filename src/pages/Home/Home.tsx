908ce49b01e1e2b2bbfed5fa8846a21a54bdfca1
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-19 20:40:16 -0800

    splash page for portfolio

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,1 +1,41 @@
-export const Home = () => (<></>)
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import styles from "./styles.module.css";
+
+const SENTENCE = "Hi my name is Daniel and I'm a Frontend Software Engineer.";
+
+export const Home = () => {
+  const [text, setText] = useState("");
+  const [index, setIndex] = useState(0);
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    if (index < SENTENCE.length) {
+      const timeout = setTimeout(() => {
+        setText((prev) => prev + SENTENCE[index]);
+        setIndex((prev) => prev + 1);
+      }, 100);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [index, SENTENCE]);
+
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(cursorInterval);
+  }, []);
+
+  return (
+    <Box className={styles.container}>
+      <Box>
+        <span className={styles.textStyle}>
+          {text}
+          {showCursor ? "|" : "\u00A0"}
+        </span>
+      </Box>
+    </Box>
+  );
+};
