1651863db8589798f11765ff1ea32252a3d523ff
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-19 20:39:41 -0800

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "pages/Home";
+import { Navbar } from "components/Navbar";
+import { pages } from "./consts";
+
+const LOGO = "DANIEL K NGUYEN";
 
 export const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
+  <>
+    <Navbar logo={LOGO} pages={pages} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </>
 );
