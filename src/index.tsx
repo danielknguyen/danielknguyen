11c06e043cf34d256cf4d20537fe64b0bf337715
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-19 20:38:21 -0800

    import roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import "@fontsource/roboto";
+import "@fontsource/roboto/900.css";
 import "./index.css";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-        <App />
+    <App />
   </React.StrictMode>
 );
