26e1141455597e4c03dedd5f87263f1e9576228b
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-20 01:30:28 -0800

    fix typo

--- src/pages/portfolio/logic/portfolioData.ts
+++ src/pages/portfolio/logic/portfolioData.ts
@@ -5,7 +5,7 @@ export const PortfolioData = [
     title: "VinTransfr",
     alt: "VinTransfr Logo",
     description:
-      "Software solution for bridging the hap netween charity and generosity in Vietnam",
+      "Software solution for bridging the gap between charity and generosity in Vietnam",
     image: vintransfrLogo,
     link: "https://vintransfr.com/",
   },
