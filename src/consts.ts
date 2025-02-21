c1bbfc7eb40ca21962085f8edc409511a6bc380c
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-21 12:42:27 -0800

    add Blogs to navigation links enum

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
-export type Page = "Home" | "Portfolio";
+export type Page = "Home" | "Portfolio" | "Blogs";
 
-export const pages: Page[] = ["Home", "Portfolio"];
+export const pages: Page[] = ["Home", "Portfolio", "Blogs"];
 
 export enum NavigationLinks {
   "Home" = "/",
   "Portfolio" = "/portfolio",
+  "Blogs" = "/blogs",
 }
