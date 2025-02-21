7d8bab3ad5c3f60d15a67b0c2de4c4dadc2ff9a0
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-21 15:04:06 -0800

    fetch single blog

--- src/services/modules/blogsApi.ts
+++ src/services/modules/blogsApi.ts
@@ -12,6 +12,18 @@ export class BlogsApi {
         throw error;
       });
   }
+
+  async getBlog(id: string): Promise<Response> {
+    return api
+      .get(`/posts/${id}`)
+      .then((results) => {
+        return results;
+      })
+      .catch((error) => {
+        console.log(error);
+        throw error;
+      });
+  }
 }
 
 export const blogsApi = new BlogsApi();
