93fedcdc75fd4893236b029c95bb1d3b2b6c7fdf
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-21 15:02:11 -0800

    include route for blog

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "pages/home";
 import { Portfolio } from "pages/portfolio";
 import { Blogs } from "pages/blogs";
+import { Blog } from "pages/blog";
 import { Navbar } from "components/Navbar";
 import { pages } from "./consts";
 
@@ -14,6 +15,7 @@ export const App = () => (
       <Route path="/" element={<Home />} />
       <Route path="/portfolio" element={<Portfolio />} />
       <Route path="/blogs" element={<Blogs />} />
+      <Route path="/blog/:slug/:id" element={<Blog />} />
     </Routes>
   </Router>
 );
