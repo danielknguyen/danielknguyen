d7eb542a09e68f528bd2cae4666fbdb6d763b8e4
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-21 16:57:46 -0800

    render project list from wordpress

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,18 +1,60 @@
+import { useEffect, useState } from "react";
+import DOMPurify from "dompurify";
+import { pagesApi } from "services/modules/pagesApi";
 import { Box, Typography } from "@mui/material";
 import { Card } from "components/Card";
+import { NavigationLinks } from "../../consts";
+import { Project } from "pages/project";
 import styles from "./styles.module.css";
-import { PortfolioData } from "pages/portfolio/logic";
 
 export const Portfolio = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const results = await pagesApi.getProjects();
+      const projects = await results.json();
+
+      const mappedProjects = projects.map((project: any) => {
+        const {
+          title: { rendered: title },
+          excerpt: { rendered: description },
+          _embedded: {
+            "wp:featuredmedia": [{ source_url: image, alt_text: alt }],
+          },
+          slug,
+          id,
+        } = project;
+
+        return {
+          title,
+          description: (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(description),
+              }}
+            />
+          ),
+          alt,
+          image,
+          slug,
+          id,
+          link: `${NavigationLinks.Project}/${slug}/${id}`,
+        };
+      });
+      setProjects(mappedProjects);
+    })();
+  }, []);
+
   return (
     <Box className={styles.container}>
       <Box className={styles.inner}>
         <Typography variant="h2" className={styles.title}>
-          Portfolio
+          Projects
         </Typography>
         <Box className={styles.grid}>
-          {PortfolioData.map((card, index) => (
-            <Card {...card} />
+          {projects.map((project) => (
+            <Card key={project.id} {...project} />
           ))}
         </Box>
       </Box>
